316b0b38c777abacfbe59f894012e1960f594de5
tGodson <tendongze95@gmail.com>
2022-02-07 23:01:11 +0100

    callbackFunctions

---
+++ callbackFunction.js
@@ -0,0 +1,10 @@
+const mainFunction = (arr, callback) => {
+    arr.push(100);
+    callback();
+}
+
+const arr = [1,2,3,4,5];
+
+mainFunction(arr, function() {
+console.log("you did it", arr);
+});
